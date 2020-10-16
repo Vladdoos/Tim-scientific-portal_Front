4f818fe2c622738dff04b6668fb4eef48328765c
Влад <wladisalw31011998@mail.ru>
2020-10-16 21:45:00 +0300

    The "research Directions" page is collapsed, a preloader for hexagons is made

---
+++ src/components/routComponents/MainPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <Header :modifiers="modifiers.header"/>
+  <MainPageBody/>
+<!--  <ProgramsPageBody/>-->
+  <Footer/>
+</template>
+
+<script>
+import Header from "../unitComponents/pattern/header/Header";
+import MainPageBody from "../unitComponents/body/controls/MainPageBody";
+import Footer from "../unitComponents/pattern/footer/Footer";
+// import ProgramsPageBody from "../unitComponents/body/controls/ProgramsPageBody";
+
+export default {
+  name: "MainPage",
+  components: {
+    // ProgramsPageBody,
+    Header,
+    MainPageBody,
+    Footer
+  },
+  data() {
+    return {
+      modifiers: {
+        header: ["header_border-none", "pizda"]
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+.swiper_slider_button_next {
+  position: relative;
+  height: fit-content;
+  left: 3%;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+.swiper_slider_button_prev {
+  position: relative;
+  height: fit-content;
+  right: 3%;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
